2e629528efff18983de4b6b9e0ddfb8ebc51a1b4
Marcin <gasiorek.mail@gmail.com>
2023-08-03 11:55:32 +0200

    created all Rest endpoints in Controller

--- src/main/java/pl/marcindev/webfluxreactivedemo/controller/ProductController.java
+++ src/main/java/pl/marcindev/webfluxreactivedemo/controller/ProductController.java
@@ -1,4 +1,44 @@
 package pl.marcindev.webfluxreactivedemo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import pl.marcindev.webfluxreactivedemo.dto.ProductDto;
+import pl.marcindev.webfluxreactivedemo.service.ProductService;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("/products")
 public class ProductController {
+    @Autowired
+    private ProductService productService;
+
+    @GetMapping
+    public Flux<ProductDto> getProducts() {
+        return productService.getProducts();
+    }
+
+    @GetMapping("/{id}")
+    public Mono<ProductDto> getProduct(@PathVariable String id) {
+        return productService.getProduct(id);
+    }
+
+    @GetMapping("/product-range")
+    public Flux<ProductDto> getProductsBetweenRange(@RequestParam("min") double min, @RequestParam("max") double max) {
+        return productService.getProductInPriceRange(min, max);
+    }
+
+    @PostMapping
+    public Mono<ProductDto> saveProduct(@RequestBody Mono<ProductDto> productDtoMono) {
+        return productService.saveProduct(productDtoMono);
+    }
+    @PutMapping("/update/{id}")
+    public Mono<ProductDto> updateProduct(@RequestBody Mono<ProductDto> productDtoMono, @PathVariable String id) {
+        return productService.updateProduct(productDtoMono,id);
+    }
+    @DeleteMapping("/delete/{id}")
+    public Mono<Void> deleteProduct(@PathVariable String id){
+        return productService.deleteProduct(id);
+    }
+
 }
